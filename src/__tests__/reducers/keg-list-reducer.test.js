be10b1b234a7e6703787dbc3ff2ada1851942612
Megan Hepner <megan.hepner@gmail.com>
2020-09-18 09:12:19 -0700

    pass test 'Should successfully update a keg'

--- src/__tests__/reducers/keg-list-reducer.test.js
+++ src/__tests__/reducers/keg-list-reducer.test.js
@@ -13,16 +13,20 @@ describe('kegListReducer', () => {
   }
 
   const currentState = {
-    1: { name: 'Level Up Pils',
+    1: { 
+    name: 'Level Up Pils',
     brand: 'Level Beer',
     price: '5.00',
     alcoholContent: '5',
-    id: 1 },
-    2: { name: 'Lite',
+    id: 1 
+    },
+    2: {
+    name: 'Lite',
     brand: 'Budweiser',
     price: '3.00',
     alcoholContent: '3',
-    id: 2 }
+    id: 2 
+    }
   }
 
   test('Should return default state if there is no action type passed into the reducer', () => {
@@ -56,15 +60,49 @@ describe('kegListReducer', () => {
       id: 1
     };
     expect(kegListReducer(currentState, action)).toEqual({
-      2: { name: 'Lite',
+      2: { 
+      name: 'Lite',
       brand: 'Budweiser',
       price: '3.00',
       alcoholContent: '3',
-      id: 2 }
+      id: 2 
+      }
     });
   });
-  
+
+  test('Should successfully update a keg', () => {
+    const { name, brand, price, alcoholContent, id } = kegData;
+    action = {
+      type: 'ADD_KEG',
+      name: name,
+      brand: brand,
+      price: price,
+      alcoholContent: alcoholContent,
+      id: id
+    }
+
+    const stateToUpdate = kegListReducer({}, action);
+
+    const updateAction =  {
+      type: 'ADD_KEG',
+      name: 'German Pilz',
+      brand: 'Zoiglhaus',
+      price: '4.00',
+      alcoholContent: '4',
+      id: 1
+    }
+    expect(kegListReducer(stateToUpdate, updateAction)).toEqual({
+      [id] : {
+        name: 'German Pilz',
+        brand: 'Zoiglhaus',
+        price: '4.00',
+        alcoholContent: '4',
+        id: 1
+      }
+    });
+  });
 });
+
 
 // name: PropTypes.string,
 // brand: PropTypes.string,
